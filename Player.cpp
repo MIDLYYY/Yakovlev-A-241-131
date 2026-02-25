bfb2ffe8fee56db72a86a00dbb402109d5659638
MIDLYYY <yakovlev.al1ak5andr@yandex.ru>
2026-02-25 20:51:39 +0300

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,9 @@
+#include "Player.h"
+
+void Player::printInfo()
+{
+    cout << "\nИнформация об игроке:" << endl;
+    cout << "ID: " << id << endl;
+    cout << "Логин: " << login << endl;
+    cout << "Пароль: " << password << endl;
+}
